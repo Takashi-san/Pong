6bb8f0dde2d455bfe706c18a05441034a0b3555e
bruno takashi <bt.tengan@gmail.com>
2020-01-07 02:15:45 -0300

    Mecânica básica do pong.

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D _rb = null;
+    [SerializeField] float _velocity = 0;
+    [Range(0.0f, 10.0f)] [SerializeField] float _angleControl = 1;
+
+    void Start()
+    {
+        _rb.velocity = Vector2.right * _velocity;
+    }
+
+    public void NewDirection(float position)
+    {
+        Vector2 newVelocity;
+
+        newVelocity.x = -Mathf.Sign(_rb.velocity.x);
+        newVelocity.y = ((transform.position.y - position) / _angleControl);
+        newVelocity *= _velocity;
+
+        _rb.velocity = newVelocity;
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D _rb = null;
+    [SerializeField] float _velocity = 0;
+    [SerializeField] float _mapLimit = 6;
+    float _horizontal;
+
+    private void Update()
+    {
+        _horizontal = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = Vector2.up * _velocity * _horizontal;
+        if (Mathf.Sign(_rb.velocity.y) > 0)
+        {
+            if (transform.position.y > _mapLimit)
+            {
+                _rb.velocity = Vector2.zero;
+            }
+        }
+        else
+        {
+            if (transform.position.y < -_mapLimit)
+            {
+                _rb.velocity = Vector2.zero;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Ball ball = other.GetComponent<Ball>();
+        if (ball)
+        {
+            ball.NewDirection(transform.position.y);
+        }
+    }
+}
